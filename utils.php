c533b63d6f47826e85548f443a64f394c467ef6c
zuxiso <zuxiso@gmail.com>
2016-03-04 11:35:54 +0800

    Initial commit

--- utils.php
+++ utils.php
@@ -3,8 +3,7 @@ if(!extension_loaded('utils')) {
 	dl('utils.' . PHP_SHLIB_SUFFIX);
 }
 $uuid = new uuid();
-var_dump(msectime())."\n";
-$uuid->setEpoch(msectime());
+$uuid->setEpoch(1456814586460);
 $uuid->setMachine(100);
 
 
@@ -12,9 +11,5 @@ for ($i = 0; $i < 10; ++$i) {
     print $uuid->getGenerate()."\n";
 }
 
-function msectime() {
-    list($tmp1, $tmp2) = explode(' ', microtime());
-    return (float)sprintf('%.0f', (floatval($tmp1) + floatval($tmp2)) * 1000);
-}
 
 ?>
